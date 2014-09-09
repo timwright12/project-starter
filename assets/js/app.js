70b7424384a18a2bbcf8ef2e5b9f4eb085653271
Tim Wright <timwright12@gmail.com>
2014-09-09 14:57:06 -0400

    init commit

---
+++ assets/js/app.js
@@ -0,0 +1,20 @@
+(function (w, doc) {
+
+  // Enable strict mode
+  "use strict";
+
+  // Local object for method references
+  var App = {};
+
+  // Namespace
+  App.ns = "Application";
+
+  // Start defining methods here
+  App.init = function() {
+
+  };
+  
+  // Start the application
+  App.init();
+
+} )( this, this.document );
