da4047948790d64496f18a98d02a8b30333b13b5
Tim Wright <timwright12@gmail.com>
2016-07-07 11:34:05 -0400

    Added App.debounce() method

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,6 +13,35 @@
   App.ns = "Application";
   
   /*
+    Debounce
+
+    - Usage - 
+    var myEfficientFn = debounce(function() {
+	    // All the taxing stuff you do
+    }, 250);
+    
+    window.addEventListener('resize', myEfficientFn);
+
+  */
+
+  App.debounce = function ( func, wait, immediate ) {
+    
+  	var timeout;
+	  return function() {
+		  var context = this, args = arguments;
+		  var later = function() {
+		 	  timeout = null;
+			  if (!immediate) func.apply(context, args);
+		  };
+		  var callNow = immediate && !timeout;
+		  clearTimeout(timeout);
+		  timeout = setTimeout(later, wait);
+		  if (callNow) func.apply(context, args);
+	  };
+	  
+  };
+  
+  /*
     Utility Ajax Call
   */
   
