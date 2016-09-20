240743611a07a671591a0af1203860afb3e3522f
Tim Wright <timwright12@gmail.com>
2016-09-20 07:45:02 -0400

    added flexbox mixins

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,47 +1,31 @@
 ;(function ($, w, doc) {
-  
+
   // Uncomment this if you're using jQuery
   // if(! 'jQuery' in w){ return; }
-  
+
   // Enable strict mode
   "use strict";
-
-	// Polyfill for el.matches
-	if (!Element.prototype.matches) {
-		Element.prototype.matches =
-		Element.prototype.matchesSelector ||
-		Element.prototype.mozMatchesSelector ||
-		Element.prototype.msMatchesSelector ||
-		Element.prototype.oMatchesSelector ||
-		Element.prototype.webkitMatchesSelector ||
-		function(s) {
-			var matches = (this.document || this.ownerDocument).querySelectorAll(s),
-			i = matches.length;
-			while (--i >= 0 && matches.item(i) !== this) {}
-			return i > -1;
-		};
-	}
 
   // Local object for method references
   var App = {};
 
   // Namespace
   App.ns = "Application";
-  
+
   /*
     Debounce (https://davidwalsh.name/essential-javascript-functions)
 
-    - Usage - 
+    - Usage -
     var myEfficientFn = debounce(function() {
 	    // All the taxing stuff you do
     }, 250);
-    
+
     window.addEventListener('resize', myEfficientFn);
 
   */
 
   App.debounce = function ( func, wait, immediate ) {
-    
+
   	var timeout;
 	return function() {
 	  var context = this, args = arguments;
@@ -54,15 +38,15 @@
 	  timeout = setTimeout(later, wait);
 	  if (callNow) func.apply(context, args);
 	};
-	  
+
   };
-  
+
   /*
     Utility Ajax Call
   */
-  
+
   App.get = function ( options, callback ) {
-    
+
     var defaults = {
         'url' : null,
         'type' : 'GET',
@@ -71,19 +55,19 @@
     request = new XMLHttpRequest(),
     httpresponse,
     i;
-    
+
     // map all default settings to user defined options
     for (i = 0; i < defaults.length; i = i + 1) {
         if( typeof options[i] === "undefined" ) {
             options[i] = defaults[i];
         }
     }
-    
+
     request.onreadystatechange = function() {
-        
+
         // check to see if the Ajax call went through
         if ( request.readyState === 4 && request.status === 200 ) {
-            
+
             // save the ajax response to a variable
             if(options.dataType === 'json'){
                 httpresponse = JSON.parse(request.responseText);
@@ -92,43 +76,43 @@
             } else {
                 httpresponse = request.responseText;
             }
-            
+
             // make sure the callback is indeed a function before executing it
             if(typeof callback === 'function'){
-            
+
                 callback(httpresponse);
-            
+
             } // end check
-    
+
         } // end ajax status check
-    
+
     }; // end onreadystatechange
-    
+
     request.open(options.type, options.url, true);
     request.send(null);
-    
+
   };
-  
+
   /*
     Check if an element is hidden (display: none OR visibility: hidden)
   */
-  
+
   App.isHidden = function ( el ) {
     return (el.offsetParent === null);
   };
-  
+
   /*
     Cross-browser way to tell if an element has a certain class
   */
-  
+
   App.hasClass = function ( el, cls ) {
     return el.className && new RegExp("(\\s|^)" + cls + "(\\s|$)").test( el.className );
   };
-  
-  /* 
+
+  /*
     Cross-browser way to add a class
   */
-  
+
   App.addClass = function ( el, cls ) {
     if ( el.classList ) {
 	    el.classList.add( cls );
@@ -136,11 +120,11 @@
 		  el.className += " " + cls;
 	  }
   };
-  
-  /* 
+
+  /*
     Cross-browser way to remove a class
   */
-  
+
   App.removeClass = function ( el, cls ) {
     if (el.classList) {
       el.classList.remove(cls);
@@ -149,12 +133,12 @@
       el.className = el.className.replace( reg, ' ' );
     }
   };
-  
+
   // Start defining methods here
   App.init = function() {
 
   };
-  
+
   // Start the application
   App.init();
 
