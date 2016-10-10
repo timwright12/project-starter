e63e4d1f39eabfe9ddf3458e4a2dc934d7ef022e
Tim Wright <timwright12@gmail.com>
2016-10-10 08:18:37 -0400

    Update app.utilities.js

--- assets/js/app.utilities.js
+++ assets/js/app.utilities.js
@@ -1,127 +1,135 @@
-// Local object for method references
-var Util = {};
+(function() {
 
-// Namespace
-Util.ns = "Utility Methods";
+	'use strict';
+  
+  // Define global Util object if it doesn't exist
+  if ( 'object' !== typeof window.Util ) {
+    window.Util = {};
+  }
 
-/*
-  Debounce (https://davidwalsh.name/essential-javascript-functions)
+  // Namespace
+  Util.ns = "Utility Methods";
 
-  - Usage -
-  var myEfficientFn = debounce(function() {
-    // All the taxing stuff you do
-  }, 250);
+  /*
+    Debounce (https://davidwalsh.name/essential-javascript-functions)
 
-  window.addEventListener('resize', myEfficientFn);
+    - Usage -
+    var myEfficientFn = debounce(function() {
+      // All the taxing stuff you do
+    }, 250);
 
-*/
+    window.addEventListener('resize', myEfficientFn);
 
-Util.debounce = function ( func, wait, immediate ) {
+  */
 
-  var timeout;
-  return function() {
-    var context = this, args = arguments;
-    var later = function() {
-      timeout = null;
-      if (!immediate) func.apply(context, args);
+  Util.debounce = function ( func, wait, immediate ) {
+
+    var timeout;
+    return function() {
+      var context = this, args = arguments;
+      var later = function() {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      var callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
     };
-    var callNow = immediate && !timeout;
-    clearTimeout(timeout);
-    timeout = setTimeout(later, wait);
-    if (callNow) func.apply(context, args);
+
   };
 
-};
+  /*
+    Utility Ajax Call
+  */
 
-/*
-  Utility Ajax Call
-*/
+  Util.get = function ( options, callback ) {
 
-Util.get = function ( options, callback ) {
+    var defaults = {
+        'url' : null,
+        'type' : 'GET',
+        'dataType' : 'html'
+    },
+    request = new XMLHttpRequest(),
+    httpresponse,
+    i;
 
-  var defaults = {
-      'url' : null,
-      'type' : 'GET',
-      'dataType' : 'html'
-  },
-  request = new XMLHttpRequest(),
-  httpresponse,
-  i;
+    // map all default settings to user defined options
+    for (i = 0; i < defaults.length; i = i + 1) {
+        if( typeof options[i] === "undefined" ) {
+            options[i] = defaults[i];
+        }
+    }
 
-  // map all default settings to user defined options
-  for (i = 0; i < defaults.length; i = i + 1) {
-      if( typeof options[i] === "undefined" ) {
-          options[i] = defaults[i];
-      }
-  }
+    request.onreadystatechange = function() {
 
-  request.onreadystatechange = function() {
+        // check to see if the Ajax call went through
+        if ( request.readyState === 4 && request.status === 200 ) {
 
-      // check to see if the Ajax call went through
-      if ( request.readyState === 4 && request.status === 200 ) {
+            // save the ajax response to a variable
+            if(options.dataType === 'json'){
+                httpresponse = JSON.parse(request.responseText);
+            } else if(options.dataType === 'xml'){
+                httpresponse = request.responseXML;
+            } else {
+                httpresponse = request.responseText;
+            }
 
-          // save the ajax response to a variable
-          if(options.dataType === 'json'){
-              httpresponse = JSON.parse(request.responseText);
-          } else if(options.dataType === 'xml'){
-              httpresponse = request.responseXML;
-          } else {
-              httpresponse = request.responseText;
-          }
+            // make sure the callback is indeed a function before executing it
+            if(typeof callback === 'function'){
 
-          // make sure the callback is indeed a function before executing it
-          if(typeof callback === 'function'){
+                callback(httpresponse);
 
-              callback(httpresponse);
+            } // end check
 
-          } // end check
+        } // end ajax status check
 
-      } // end ajax status check
+    }; // end onreadystatechange
 
-  }; // end onreadystatechange
+    request.open(options.type, options.url, true);
+    request.send(null);
 
-  request.open(options.type, options.url, true);
-  request.send(null);
+  };
 
-};
+  /*
+    Check if an element is hidden (display: none OR visibility: hidden)
+  */
 
-/*
-  Check if an element is hidden (display: none OR visibility: hidden)
-*/
+  Util.isHidden = function ( el ) {
+    return (el.offsetParent === null);
+  };
 
-Util.isHidden = function ( el ) {
-  return (el.offsetParent === null);
-};
+  /*
+    Cross-browser way to tell if an element has a certain class
+  */
 
-/*
-  Cross-browser way to tell if an element has a certain class
-*/
+  Util.hasClass = function ( el, cls ) {
+    return el.className && new RegExp("(\\s|^)" + cls + "(\\s|$)").test( el.className );
+  };
 
-Util.hasClass = function ( el, cls ) {
-  return el.className && new RegExp("(\\s|^)" + cls + "(\\s|$)").test( el.className );
-};
+  /*
+    Cross-browser way to add a class
+  */
 
-/*
-  Cross-browser way to add a class
-*/
+  Util.addClass = function ( el, cls ) {
+    if ( el.classList ) {
+      el.classList.add( cls );
+    } else if (!Util.hasClass( el, cls )) {
+      el.className += " " + cls;
+    }
+  };
 
-Util.addClass = function ( el, cls ) {
-  if ( el.classList ) {
-    el.classList.add( cls );
-  } else if (!Util.hasClass( el, cls )) {
-	  el.className += " " + cls;
-  }
-};
+  /*
+    Cross-browser way to remove a class
+  */
 
-/*
-  Cross-browser way to remove a class
-*/
-
-Util.removeClass = function ( el, cls ) {
-  if (el.classList) {
-    el.classList.remove(cls);
-  } else if (Util.hasClass(el, cls)) {
-    var reg = new RegExp('(\\s|^)' + cls + '(\\s|$)');
-    el.className = el.className.replace( reg, ' ' );
-  }
-};
+  Util.removeClass = function ( el, cls ) {
+    if (el.classList) {
+      el.classList.remove(cls);
+    } else if (Util.hasClass(el, cls)) {
+      var reg = new RegExp('(\\s|^)' + cls + '(\\s|$)');
+      el.className = el.className.replace( reg, ' ' );
+    }
+  };
+ 
+} )();
